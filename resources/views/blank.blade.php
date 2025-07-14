fdb1a294973cb62c0b81a803f911395cf086442b
SamerAljbour <samerseran34@gmail.com>
2025-07-14 11:26:42 +0300

    Add frontend layout files including header, footer, and main app structure
    - Created app.blade.php for the main layout structure.
    - Added footer.blade.php with contact information and a contact form.
    - Implemented header.blade.php with navigation links and branding.

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,8 @@
+@extends('partials.frontend.app')
+@section('section')
+           <div class="container ts-page-wrapper">
+            <div class="row">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur praesentium sunt reiciendis a facilis, corporis earum hic aliquam nihil aut, fugiat quibusdam omnis eligendi atque dolorum, magnam at vitae minima.</p>
+            </div>
+           </div>
+@endsection
---
+++ resources/views/partials/frontend/app.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="ThemeStarz">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/font-awesome/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <title>StartUps - Startup Landing Page</title>
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar" class="has-loading-screen">
+    <div class="ts-page-wrapper" id="page-top">
+
+        <header>
+            @include('partials.frontend.header')
+        </header>
+
+        <main id="ts-content">
+            @yield('section')
+
+
+        </main>
+        <!--end #content-->
+        @include('partials.frontend.footer')
+    </div>
+    <!--end page-->
+
+
+    <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyBEDfNcQRmKQEyulDN8nGWjLYPm8s4YB58"></script>
+    <script src="{{ asset('assets/js/isInViewport.jquery.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/js/scrolla.jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery-validate.bootstrap-tooltip.min.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+
+
+
+
+    @stack('scripts')
+
+</body>
+
+</html>
